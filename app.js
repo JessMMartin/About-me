d445c3a14c70065898553d195acbfa09e1eda3ea
Jess <jessicammartin@live.co.uk>
2023-06-15 10:57:25 +0100

    Javascript update

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 "use strict";
 
-alert("Welcome to my website");
+alert("⭐Welcome to my website⭐");
 
-let dino = prompt("What is your favourite dinosaur?")
+let dino = prompt("What is your favourite dinosaur?🦕")
     console.log(dino);
-    alert("Nice, mine is Allosaurus!")
+    alert("Nice, mine is Allosaurus!🦖")
 
 function foodName(){
     let foodName = prompt("Is Gazpacho a Spanish dish? Please answer yes or no only" +"(yes or no)").toLowerCase();
     if (foodName.toLowerCase() === "yes") {
-       alert("Yes, that's right")
+       alert("Yes, that's right!")
     }else if (foodName.toLowerCase() === "no") {
-        alert("Actually, it is a cold vegetable spanish soup")
+        alert("Actually, it is a cold vegetable spanish soup.🥣")
     }else {
         alert("Please enter 'yes' or 'no'");
     }; 
 
 }
     foodName()
+
+const number = Math.floor(Math.random() * 10 + 1);
+console.log(number);
+
+for (let i = 2; i >= 0; i--) {
+  let guesses = i + 1;
+
+  let guess = prompt("Guess a number between 1 and 10! you have " + guesses + " attempts");
+
+  const guessNum = parseInt(guess);
+
+  if (guessNum === number) {
+    alert("🥳 You win! 🎉");
+    break;
+  } else if (guessNum < number) {
+    alert("too low");
+  } else if (guessNum > number) {
+    alert("too high");
+  }
+
+  if (i === 0) {
+    alert("The correct number was " + number);
+    break;
+  }
+}
